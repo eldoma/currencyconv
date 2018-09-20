c1eef19e8d36b0b486e464e8e2417c29f1aba850
Eldo Martadjaya <eldoma@gmail.com>
2018-09-20 17:16:07 +0700

    Currency Converter uploaded
    Uploaded Currency Converter files, see Readme for instruction how to run

---
+++ src/js/data/Data.js
@@ -0,0 +1,55 @@
+const data = {
+    "currencies": [
+       
+        {
+            "code": "USD",
+            "sellRate": 1,
+            "name": "United States Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "IDR",
+            "sellRate": 14848,
+            "name": "Indonesian Rupiah",
+            "sign": "$"
+        },
+        {
+            "code": "AUD",
+            "sellRate": 1.38,
+            "name": "Australian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "CAD",
+            "sellRate": 1.29,
+            "name": "Canadian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "NZD",
+            "sellRate": 1.50,
+            "name": "New Zealand Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "JPY",
+            "sellRate": 112.24,
+            "name": "Japanese Yen",
+            "sign": "¥"
+        },
+        {
+            "code": "GBP",
+            "sellRate": .76,
+            "name": "British Pounds Sterling",
+            "sign": "£"
+        },
+        {
+            "code": "EUR",
+            "sellRate": .85,
+            "name": "European Union Euro",
+            "sign": "€"
+        }
+    ]
+}
+
+export default data;
